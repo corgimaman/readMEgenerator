bbad99e47942c6fe22f9347dfcab35f4e8ee7f67
romantic <ida.shalilian@gmail.com>
2021-04-25 12:38:08 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'Describe your project:',
+    'How do you install your project?',
+    'How do you use your project/What are the instructions for use?',
+    'Do you have any contribution guidelines?',
+    'What are your contribution guidelines?',
+    'What are the test instructions?',
+    'Which license would you like to associate to this project?',
+    'Enter your GitHub username:',
+    'What e-mail address would you like to put for people to contact you with additional questions?'
+];
+
+const licenses = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
